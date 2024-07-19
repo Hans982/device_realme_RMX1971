0eb6f8f411ecbc18366272b84023dc885d8d8051
Hans98 <142167463+Hans982@users.noreply.github.com>
2024-07-19 17:47:45 +0700

    Update setup-makefiles.sh
    Memek

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -34,3 +34,7 @@ write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers
+
+# Trying Signed Build
+rm -rf vendor/lineage-priv
+git clone https://github.com/Tiktodz/vendor -b matrixx vlp && cp -R vlp/* vendor/ && rm -rf vlp
