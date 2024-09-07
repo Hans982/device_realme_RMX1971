b3b38e3cc42bbc0a0f84c3765d5e5e7645ef1eec
Hans98 <142167463+Hans982@users.noreply.github.com>
2024-09-07 23:56:33 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,3 @@ git clone --depth=1 -b 14-r5p https://github.com/Hans982/android_kernel_realme_s
 # Clang Missing Fixups
 rm -rf prebuilts/clang/host/linux-x86/clang-proton
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
-
-# Trying Signed Build
-rm -rf vendor/lineage-priv
-git clone https://github.com/ardia-kun/vendor -b 14 vlp && cp -R vlp/* vendor/ && rm -rf vlp
