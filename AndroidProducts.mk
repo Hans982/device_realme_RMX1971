73d612647dd7a2f2cdb83d4e31ed5e8664cefb32
Hans98 <142167463+Hans982@users.noreply.github.com>
2024-10-25 22:25:30 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/infinity_RMX1971.mk
 
 COMMON_LUNCH_CHOICES := \
-   infinity_RMX1971-ap2a-eng \
-   infinity_RMX1971-ap2a-user \
-   infinity_RMX1971-ap2a-userdebug
+   infinity_RMX1971-ap2a-userdebug \
