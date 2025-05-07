c6745f17f660ef153765507c9be2ee3e5bfd5710
Hans98 <142167463+Hans982@users.noreply.github.com>
2025-05-07 17:52:00 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1971.mk
+    $(LOCAL_DIR)/orion_RMX1971.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1971-userdebug \
-    lineage_RMX1971-eng
+    orion_RMX1971-ap2a-userdebug \
