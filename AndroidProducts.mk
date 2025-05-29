373194f01048dcdd4c7fdd1878d7d2fd8b2e17e3
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-05-29 16:21:37 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1971.mk
+    $(LOCAL_DIR)/alpha_RMX1971.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1971-user \
-    lineage_RMX1971-userdebug \
-    lineage_RMX1971-eng
+    alpha_RMX1971-user \
+    alpha_RMX1971-userdebug \
+    alpha_RMX1971-eng
