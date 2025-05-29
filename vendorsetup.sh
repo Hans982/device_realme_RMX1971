0a04c9a8ec064b1e4f0b3687ac4e56d9c2bf271e
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-05-29 16:23:46 +0700

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf kernel/realme/sdm710
+git clone --recursive https://github.com/Hans982/android_kernel_realme_sdm710 -b 14-r5p-ksu kernel/realme/sdm710
+
+export BUILD_USER=Hans982
+export TZ=Asia/Jakarta
