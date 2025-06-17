d4310775850426c903f096084b19b8b8b6968e4a
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-06-17 15:00:57 +0700

    Update and rename lineage_RMX1971.mk to bliss_RMX1971.mk

--- bliss_RMX1971.mk
+++ bliss_RMX1971.mk
@@ -4,21 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common DerpFest stuff
+# Inherit some common Bliss stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from RMX1971 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Official-ify
-DERPFEST_BUILD_TYPE := Official
-DERPFEST_BUILD_VARIANT := Stable
-
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1971
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1971
+PRODUCT_NAME := bliss_RMX1971
 PRODUCT_MODEL := RMX1971
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
